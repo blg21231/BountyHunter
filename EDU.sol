2690866b29ee5fc39e6e0623fcd4485160f4f3a7
blg21231 <39961911+blg21231@users.noreply.github.com>
2022-04-24 20:51:03 -0400

    Update EDU.sol

--- EDU.sol
+++ EDU.sol
@@ -3,8 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Bounty.sol"
 
-contract EDU is ERC20 {
+contract EDU is ERC20, Ownable {
     constructor(uint256 0) ERC20("EDU", "EDU") {
     }
+    Bounty b;
+    transferOwnership(b.getAddress());
 }
