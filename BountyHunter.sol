75bc4b3c7cecf7d35c30f334e4ca28caa0db9e96
blg21231 <39961911+blg21231@users.noreply.github.com>
2022-04-25 17:58:11 -0400

    Update BountyHunter.sol

--- BountyHunter.sol
+++ BountyHunter.sol
@@ -13,8 +13,11 @@ contract BountyHunter is Ownable {
   uint randNonce = 0;
   
   mapping (address => boolean) public isStudent;
-  mapping (address => string) as email;
-  mapping (string => uint) as code;
+  mapping (address => string) email;
+  mapping (string => uint) code;
+  mapping (uint => address) codeToAddress;
+  mapping (address => uint) previousNumResponses;
+  mapping (address => uint) lastDividendClaimTime;
 
   event addressVerified(address _address);
   
@@ -50,19 +53,40 @@ contract BountyHunter is Ownable {
     randNonce++;
     uint random2 = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % 100000000;
     code[_email] = random2;
+    codeToAddress[random2] = msg.sender;
     return random2;
   }
     
   function _verifyStudent(uint _code) private {
     require(keccak256(abi.encodePacked(_code)) == keccak256(abi.encodePacked(code[email[msg.sender)));
-    isStudent[msg.sender] = True;
+    isStudent[codeToAddress[_code]] = True;
+    lastDividendClaimTime[codeToAddress[_code]] = now;
   }
   
   function _isStudent(address _address) public view returns (bool) {
     return isStudent[_address];
   }
   
-  function withdraw(address _address, uint amount) private {
+  function _withdraw(address _address, uint amount) private {
     transfer(_address, amount);
+  }
+  
+  function _claimDividends(address _address) public returns (string) {
+    string output = ""
+    if (isStudent[_address] == False) {
+      output = "Verify your .edu email and create some Responses to receive free EDU dividends!";
+    }
+    else if (numResponses[_address] - previousNumResponses[_address] < 2) {
+      output = "Create some Responses to receive free EDU dividends!";
+    }
+    else if (now - lastDividendClaimTime[_address] < 7 * 24 * 60 * 60) {
+      output = "Please wait at least 7 days after last Dividend Claim to Claim again";
+    }
+    else {
+      output = "Dividends claimed! Come back next week and create some Responses to receive more free EDU dividends!";
+      previousNumResponses[_address] = numResponses[_address];
+      lastDividendClaimTime[_address] = now;
+    return output;
+    }
   }
 }
